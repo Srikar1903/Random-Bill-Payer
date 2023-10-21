06976171c01e05e691782e3f0b14a26f6b7b2254
Srikar kv <123224795+Srikar1903@users.noreply.github.com>
2023-10-21 19:37:58 +0530

    Add files via upload

---
+++ bill_payer.py
@@ -0,0 +1,7 @@
+a=input("please enter the names separated by ',': ")
+import random
+b=a.split(",")
+print(f"{b} let me pick one")
+import random
+c=random.choice(b)
+print(f"{c} will pay the bill")
